73544197fd6ff659bc234a438321091c219ce8b3
liuchao412 <liuchao412@foxmail.com>
2017-11-06 08:47:03 +0800

    test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
 	const char msg[] = "Hello,ACE!";
 
 	ACE_DEBUG((LM_DEBUG,ACE_TEXT("(%P|%t) msg:%s\n"),msg));
-
+	ACE_DEBUG((LM_DEBUG,("Hello World\n")));
 	return 0;
 }
