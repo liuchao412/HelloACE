f6a147af2d9195e4ef21f32747d196d805d3fd04
liuchao412 <liuchao412@foxmail.com>
2017-11-06 14:04:07 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-#include <ace/OS_main.h>
-#include <ace/ACE.h>
-#include <ace/Log_Msg.h>
+#include "ace/OS_NS_sys_time.h"
+#include "ace/CDR_Stream.h"
+#include "ace/INET_Addr.h"
+#include "ace/SOCK_Connector.h"
+#include "ace/SOCK_Stream.h"
+#include "ace/Log_Msg.h"
+#include "ace/Log_Record.h"
+#include "ace/Truncate.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_stdlib.h"
 
-int main(int argc, char *argv[])
+int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
 {
-	const char msg[] = "Hello,ACE!";
-
-	ACE_DEBUG((LM_DEBUG,ACE_TEXT("(%P|%t) msg:%s\n"),msg));
-
-	ACE_DEBUG((LM_DEBUG,("Hello World\n")));
+	ACE_DEBUG((LM_INFO, "Complite OK.\n"));
 	return 0;
 }
